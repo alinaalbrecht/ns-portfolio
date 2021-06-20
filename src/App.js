154f695c66987abdb4aa269b486c9c1170493115
alinaalbrecht <alinaalbrecht@mailbox.org>
2021-06-20 09:29:41 +0200

    add background

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+/* import './App.css'; */
+import Background from './components/Background';
+
+function App() {
+  return (
+  <div>
+    <Background/>
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/Background.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../css/Background.scss"
+
+const Background = (props) => {
+    const makeCircles = () => {
+        const circles = [];
+        for (let i = 1; i <= 100; i++) {
+            circles.push(<div key={i} className="circle-container">
+            <div className="circle"></div>
+          </div>);
+        }
+        return circles;
+    }
+    
+    return (
+        <div className="container">
+            {makeCircles()}
+        </div>
+    );
+}
+
+export default Background
